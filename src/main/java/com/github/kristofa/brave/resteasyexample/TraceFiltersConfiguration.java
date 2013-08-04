169b28185295fa3b756a70adfd9ca69835797d6a
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2013-08-04 19:39:52 +0200

    Update example application to make use of new brave-impl-spring module.
    This means we can delete ClientTracerConfiguration,
    EndPointSubmitterConfiguration and ServerTracerConfiguration as they
    are provided by brave-impl-spring. We have to add
    TraceFiltersConfiguration instead. Also update documentation.

--- src/main/java/com/github/kristofa/brave/resteasyexample/TraceFiltersConfiguration.java
+++ src/main/java/com/github/kristofa/brave/resteasyexample/TraceFiltersConfiguration.java
@@ -1,23 +1,21 @@
 package com.github.kristofa.brave.resteasyexample;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import com.github.kristofa.brave.Brave;
-import com.github.kristofa.brave.EndPointSubmitter;
+import com.github.kristofa.brave.TraceFilters;
 
-/**
- * Spring dependency injection configuration for {@link EndPointSubmitter}.
- * 
- * @author kristof
- */
 @Configuration
-public class EndPointSubmitterConfiguration {
+public class TraceFiltersConfiguration {
 
     @Bean
     @Scope(value = "singleton")
-    public EndPointSubmitter endPointSubmitter() {
-        return Brave.getEndPointSubmitter();
+    public TraceFilters traceFilters() {
+        return new TraceFilters(Arrays.asList(Brave.getTraceAllTraceFilter()));
     }
+
 }
