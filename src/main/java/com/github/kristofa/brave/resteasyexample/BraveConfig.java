294d753c5dc5f82ba0066308213e06fdc75dc6a7
Kristof Adriaenssens <kristof.adriaenssens@soundcloud.com>
2015-11-25 21:51:52 +0100

    Configure KafkaSpanCollector
    Configure KafkaSpanCollector. To be able to get this to work on your machine
    you will have to 'mvn install' a version of Brave which includes the
    KafkaSpanCollector like this branch:
    https://github.com/openzipkin/brave/tree/brave-spancollector-kafka

--- src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
+++ src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
@@ -8,6 +8,8 @@ import com.github.kristofa.brave.http.DefaultSpanNameProvider;
 import com.github.kristofa.brave.http.ServiceNameProvider;
 import com.github.kristofa.brave.http.SpanNameProvider;
 import com.github.kristofa.brave.http.StringServiceNameProvider;
+import com.github.kristofa.brave.kafka.KafkaSpanCollector;
+import com.github.kristofa.brave.scribe.ScribeSpanCollector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -21,9 +23,9 @@ public class BraveConfig {
     @Scope(value = "singleton")
     public Brave brave() {
        Brave.Builder builder = new Brave.Builder();
-//       return builder.spanCollector(new KafkaSpanCollector("192.168.99.100:9092"))
-//       return builder.spanCollector(new ScribeSpanCollector("192.168.99.100", 9410))
-       return builder.spanCollector(new LoggingSpanCollector())
+        //  return builder.spanCollector(new ScribeSpanCollector("192.168.99.100", 9410))
+        //  return builder.spanCollector(new LoggingSpanCollector())
+       return builder.spanCollector(new KafkaSpanCollector("192.168.99.100:9092"))
                .traceFilters(Arrays.<TraceFilter>asList(new FixedSampleRateTraceFilter(1)))
                .build();
     }
