d944896820cd3965682953e60199a67fdc06e3fc
Adrian Cole <acole@pivotal.io>
2016-05-01 14:15:31 +0800

    Updates to brave 3.6

--- src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
+++ src/main/java/com/github/kristofa/brave/resteasyexample/BraveConfig.java
@@ -1,10 +1,11 @@
 package com.github.kristofa.brave.resteasyexample;
 
 import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.EmptySpanCollectorMetricsHandler;
 import com.github.kristofa.brave.http.DefaultSpanNameProvider;
-import com.github.kristofa.brave.http.ServiceNameProvider;
+import com.github.kristofa.brave.http.HttpSpanCollector;
 import com.github.kristofa.brave.http.SpanNameProvider;
-import com.github.kristofa.brave.http.StringServiceNameProvider;
+import com.github.kristofa.brave.kafka.KafkaSpanCollector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -15,16 +16,10 @@ public class BraveConfig {
     @Bean
     @Scope(value = "singleton")
     public Brave brave() {
-       Brave.Builder builder = new Brave.Builder();
-       //builder.spanCollector(KafkaSpanCollector.create("192.168.99.100:9092", new EmptySpanCollectorMetricsHandler()));
-       //builder.spanCollector(HttpSpanCollector.create("http://192.168.99.100:9411/", new EmptySpanCollectorMetricsHandler()));
+       Brave.Builder builder = new Brave.Builder("brave-resteasy-example");
+       builder.spanCollector(KafkaSpanCollector.create("127.0.0.1:9092", new EmptySpanCollectorMetricsHandler()));
+       //builder.spanCollector(HttpSpanCollector.create("http://127.0.0.1:9411/", new EmptySpanCollectorMetricsHandler()));
        return builder.build();
-    }
-
-    @Bean
-    @Scope(value = "singleton")
-    public ServiceNameProvider serviceNameProvider() {
-        return new StringServiceNameProvider("test_service");
     }
 
     @Bean
