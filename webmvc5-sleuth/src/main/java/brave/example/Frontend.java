b36cc84339ee96ed955888f000346b97a1d0e61a
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2024-02-09 07:41:35 +0700

    sleuth: converts to webmvc to allow eureka discovery (#112)
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- webmvc5-sleuth/src/main/java/brave/example/Frontend.java
+++ webmvc5-sleuth/src/main/java/brave/example/Frontend.java
@@ -4,29 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.web.client.RestTemplate;
+
+import static brave.example.Discovery.withDiscoveryArgs;
 
 @EnableAutoConfiguration
 @RestController
 public class Frontend {
-  final WebClient webClient;
+  final RestTemplate restTemplate;
+  final String backendEndpoint;
 
-  @Autowired Frontend(WebClient.Builder webClientBuilder,
+  @Autowired Frontend(RestTemplate restTemplate,
       @Value("${backend.endpoint:http://127.0.0.1:9000/api}") String backendEndpoint) {
-    this.webClient = webClientBuilder.baseUrl(backendEndpoint).build();
+    this.restTemplate = restTemplate;
+    this.backendEndpoint = backendEndpoint;
   }
 
-  @GetMapping("/") public Mono<String> callBackend() {
-    return webClient.get().uri("").retrieve().bodyToMono(String.class);
+  @RequestMapping("/") public String callBackend() {
+    return restTemplate.getForObject(backendEndpoint, String.class);
   }
 
   public static void main(String[] args) {
-    SpringApplication.run(Frontend.class,
+    SpringApplication.run(Frontend.class, withDiscoveryArgs(
         "--spring.application.name=frontend",
-        "--server.port=8081"
+        "--server.port=8081")
     );
   }
 }
